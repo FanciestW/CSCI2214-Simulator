13ac17f637dda825608187064e6ef09f69781b64
William Lin <wlin26@yahoo.com>
2017-04-15 18:23:23 -0400

    Initial Commit
    Started working on Project 2, the simulator. Added tools and set local working directory

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by William Lin on 4/13/17.
+//  Copyright © 2017 William Lin. All rights reserved.
+//
+
+#include "tools.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
