4eff95831d62ab425de99cd09f4a2473128ca34a
William Lin <wlin26@yahoo.com>
2017-04-30 13:16:11 -0400

    Added control signals and functions

--- Project2/Project2/Simulator.hpp
+++ Project2/Project2/Simulator.hpp
@@ -15,6 +15,8 @@ class Simulator{
 private:
     MainMemory mem;
     int registers[32] = {};
+    int DMX;
+    int rd, rs1, rs2, imm, offset = 0;
     int pc = 0;
     int ir = 0;
     int mdr = 0;
@@ -28,14 +30,62 @@ private:
     int addr = 0;
     int data = 0;
     
+    //Control Signals
+    int S2op = -1;
+    bool Zflag = false;
+    int ALUop = -1;
+    bool Cload = false;
+    bool REGload = false;
+    bool Aload = false;
+    bool Aoe = false;
+    bool Bload = false;
+    bool Boe = false;
+    int REGselect = -1;
+    bool IRload = false;
+    bool IRoeS1 = false;
+    bool IRoeS2 = false;
+    int opcode = -1;
+    int opcodeALU = -1;
+    bool Reset = false;
+    bool PCload = false;
+    bool PCoeS1 = false;
+    bool PCMARselect = 0;
+    bool MARload = false;
+    bool MemRead = false;
+    bool MDRload = false;
+    bool MDRoeS2 = false;
+    bool MemWrite = false;
+    int MemOP = -1;
+    
+    //Control Signal Functions
+    void setPCMARselect(bool);
+    void readMem(bool read, int op){ if(read) data = mem.readMemory(addr, op); }
+    void setIRload(bool);
+    void setPCoeS1(bool oe){ if(oe) s1 = pc; }
+    void setS2op(int);
+    void setALUop(int);
+    void setPCload(bool load){ if(load) pc = dest; }
+    void setAload(bool load){ if(load) a = registers[rs1]; }
+    void setAoe(bool oe){ if(oe) s1 = a; }
+    void setBload(bool load){ if(load) b = registers[rs2]; }
+    void setBoe(bool oe){ if(oe) s2 = b; }
+    void setCload(bool load){ if(load) c = dest; }
+    void setREGload(bool load){ if(load) registers[DMX] = c; }
+    void setREGselect(int);
+    
     
 public:
     ~Simulator() = default;
     Simulator(){}
     void run();
     void loadCode(string file){ mem.loadProgramCode(file); }
+    void step1();
+    void step2();
+    void runRtype();
+    void runJtype();
     void printStateHex();
     void printStateDec();
+    int getInstrType(int);
     
 };
 
