ad35d940374192f5ac3c78cdd616aedbc7307e96
William Lin <wlin26@yahoo.com>
2017-05-08 16:09:31 -0400

    Updated main to test test2

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     Simulator sim;
-    sim.loadCode("test1.txt");
+    sim.loadCode("test2.txt");
     sim.run(1);
     cout << "Simulation Complete" << endl;
 }
