fba5eb27f5c3553b8af1f55baffa280a4609ea76
William Lin <wlin26@yahoo.com>
2017-05-08 16:08:47 -0400

    Removed write to Mem (Was not used)

--- Project2/Project2/Simulator.hpp
+++ Project2/Project2/Simulator.hpp
@@ -101,7 +101,6 @@ public:
     Simulator(){}
     void run(bool);
     void loadCode(string file){ mem.loadProgramCode(file); }
-    void writeToMem(int addr, unsigned int data, int WHB){ mem.writeMemory(addr + 3000, data, WHB); }
     void printStateHex();
     void printStateDec();
     void printStatePretty();
